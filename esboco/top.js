61d2513990105e9a295f2b1bdb78c6092e358a46
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-10 06:41:42 -0300

    criando esboço

---
+++ esboco/top.js
@@ -0,0 +1,10 @@
+const toTop = document.querySelector(".arrow-top")
+
+window.addEventListener("scroll", () =>{
+    if(window.pageYOffset > 100){
+        toTop.classList.add('active')
+    }else{
+        toTop.classList.remove('active')
+    }
+
+})
