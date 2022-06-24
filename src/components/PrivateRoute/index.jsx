d5e27341911b88c46525c8ea1ca39fa87322fe08
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-24 19:38:28 -0300

    reiniciando projeto

--- src/components/PrivateRoute/index.jsx
+++
@@ -1,7 +0,0 @@
-import {Navigate, Route, Outlet } from 'react-router-dom';
-import { isAuthenticated } from '../../auth';
-
-export const PrivateRoute = ({children}) =>{
-            var auth = isAuthenticated()
-            return auth?children:<Navigate to="/login"/>
-}
