f0a5847869fe4cfbd1177b0edb83d01ec0e0f777
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-06-25 18:27:24 -0300

    api graphcms & integracao apollo

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import {Login} from './pages/Login'
 
 function App() {
   return (
-    //<Home/>
-    <Login/>
+    <Home/>
+    //<Login/>
     //<Poeta/>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,25 +7,28 @@ import { Card } from '../../components/Card'
 
 import luaImg from '../../img/pexels.jpg'
 import gitImg from '../../img/git_white.png'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_POETRYS_QUERY = gql`
+    query{
+        publishes{
+            id
+            date
+            poeta
+            poesia
+    }
+}
+`
+
+interface Publish{
+    id:string;
+    date:string;
+    poeta:string;
+    poesia:string;
+}
 
 export function Home() {
-    const autores = ['anonimo', "desconhecido","Zeus","Anfitrião","Ninguém", '5H4D0W']
-    const poesia={
-        texto: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique minus adipisci, necessitatibus vitae mollitia voluptas nemo fugit non recusandae voluptates, quibusdam dolorem! Quisquam deserunt rem ratione, nemo voluptatum atque corporis.",
-        date(){
-            return new Date().toLocaleString("pt-br", {
-                day: '2-digit',
-                month: '2-digit',
-                year: '2-digit',
-                hour: '2-digit',
-                minute:"2-digit",
-                second:'2-digit'
-            })
-        },
-        autor(){
-            return autores[Math.floor(Math.random() * autores.length)]
-        },
-    }
+    const {data} = useQuery<{publishes:Publish[]}>(GET_POETRYS_QUERY)
 
   return (
     <>
@@ -44,24 +47,19 @@ export function Home() {
             />
         </div>    
     </header>
-    <main>
-        <Card 
-            date={poesia.date()}
-            mensagem={poesia.texto}
-            autor={poesia.autor()}
-        />
-        <Card 
-            date={poesia.date()}
-            mensagem={poesia.texto}
-            autor={poesia.autor()}
-        />
-        <Card 
-            date={poesia.date()}
-            mensagem={poesia.texto}
-            autor={poesia.autor()}
-        />
+    <main>        
+     {
+        data?.publishes.map(publish=>{
+            return <Card
+                key={publish.id}
+                mensagem={publish.poesia}
+                autor={publish.poeta}
+                date={publish.date.split('-').reverse().join('-')}
+            />
+        })
+     }
+     
         
-
     </main>
     <footer> 
         <address id="contato" className="home">
